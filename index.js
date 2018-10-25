1ff062244426077fb1276e5f81e9cecc9a5ab797
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-10-25 16:06:11 -0700

    standard style fixes

--- index.js
+++ index.js
@@ -5,33 +5,31 @@ var through = require('through2')
 var nodeToWebStream = require('readable-stream-node-to-web')
 var webStreamToNode = require('./lib/web-to-node-readable-stream')
 
-var noop = function() {}
-
-var listen = function(stream, opts, cb) {
+var listen = function (stream, opts, cb) {
   if (!cb) return stream
-  eos(stream, function(err) {
+  eos(stream, function (err) {
     if (err) return cb(err)
     cb(null, opts)
   })
   return stream
 }
 
-var BlobStore = function(opts) {
-  if (!window || !window.caches) throw 'Not supported on this platform'
+var BlobStore = function (opts) {
+  if (!window || !window.caches) throw new Error('Not supported on this platform')
   if (!(this instanceof BlobStore)) return new BlobStore(opts)
   opts = opts || {}
-  if (typeof opts === 'string') opts = {name:opts}
+  if (typeof opts === 'string') opts = { name: opts }
 
   this.name = opts.name || 'blob-store'
 }
 
-BlobStore.prototype.createWriteStream = function(opts, cb) {
-  if (typeof opts === 'string') opts = {key:opts}
+BlobStore.prototype.createWriteStream = function (opts, cb) {
+  if (typeof opts === 'string') opts = { key: opts }
   if (opts.name && !opts.key) opts.key = opts.name
 
   var proxy = listen(through(), opts, cb)
 
-  var response = new Response(nodeToWebStream(proxy))
+  var response = new window.Response(nodeToWebStream(proxy))
 
   window.caches.open(this.name).then(function (cache) {
     cache.put(opts.key, response)
@@ -39,7 +37,7 @@ BlobStore.prototype.createWriteStream = function(opts, cb) {
   return proxy
 }
 
-BlobStore.prototype.createReadStream = function(key, opts) {
+BlobStore.prototype.createReadStream = function (key, opts) {
   if (key && typeof key === 'object') return this.createReadStream(key.key, key)
   var proxy = duplexify()
   proxy.setWritable(false)
@@ -56,23 +54,17 @@ BlobStore.prototype.createReadStream = function(key, opts) {
   return proxy
 }
 
-BlobStore.prototype.exists = function(opts, cb) {
-  if (typeof opts === 'string') opts = {key:opts}
+BlobStore.prototype.exists = function (opts, cb) {
+  if (typeof opts === 'string') opts = { key: opts }
   window.caches.open(this.name).then(function (cache) {
     cache.match(opts.key).then(function (response) {
       cb(null, !!response)
     })
   })
-  // if (typeof opts === 'string') opts = {key:opts}
-  // var key = join(this.path, opts.key)
-  // fs.stat(key, function(err, stat) {
-  //   if (err && err.code !== 'ENOENT') return cb(err)
-  //   cb(null, !!stat)
-  // })
 }
 
-BlobStore.prototype.remove = function(opts, cb) {
-  if (typeof opts === 'string') opts = {key:opts}
+BlobStore.prototype.remove = function (opts, cb) {
+  if (typeof opts === 'string') opts = { key: opts }
   window.caches.open(this.name).then(function (cache) {
     cache.delete(opts.key).then(function (deleted) {
       cb()
